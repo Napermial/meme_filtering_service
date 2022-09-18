37971319ec04d8e32fb095cd0c09fabf88208af6
Napermial <hatvani9823@gmail.com>
2022-09-18 21:09:39 +0200

    db as dependency, return 12 images only

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI, Depends, UploadFile, File
 from persistence.db import MetaDataBase
+from fastapi.middleware.cors import CORSMiddleware
+import base64
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = MetaDataBase()
+    yield db
 
 
 @app.get("/ping")
@@ -11,14 +30,20 @@ async def ping():
 
 @app.get("/suggest/tag/{search}")
 async def get_autocomplete_suggestions(
-    search, db: MetaDataBase = Depends(MetaDataBase)
+    search, db: MetaDataBase = Depends(get_db)
 ):
     return {"suggestions": db.get_autocomplete_suggestions(search)}
 
 
 @app.get("/suggest/image/{search}")
-async def get_image_suggestions(search, db: MetaDataBase = Depends(MetaDataBase)):
-    return {"suggestions": db.get_image_suggestions(search)}
+async def get_image_suggestions(search, db: MetaDataBase = Depends(get_db)):
+    encoded_images = []
+    for i, image_path in enumerate(db.get_image_suggestions(search)):
+        if i < 12:
+            with open(image_path, "rb") as f:
+                encoded_string = base64.b64encode(f.read())
+                encoded_images.append(encoded_string)
+    return {"suggestions": [{"path": image} for image in encoded_images]}
 
 
 @app.post("/image/upload")
